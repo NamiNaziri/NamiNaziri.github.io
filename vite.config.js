1da3135897ada9479ca65beff7daad641be7a1d3
Nami <49837425+NamiNaziri@users.noreply.github.com>
2026-07-06 04:13:48 +0300

    removed strip

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import strip from 'vite-plugin-strip';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [react(),
-    strip({
-      functions: ['console.log', 'console.debug', 'console.warn', 'console.error']
-    }),
   ],
   server: {
     proxy: {
